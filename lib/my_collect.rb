c2b72ef804de2ccbbea3f2d3f0b29d1760fa7658
Monica Marziano <mmarziano1@gmail.com>
2018-04-03 10:33:27 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ collection = []
 
 def my_collect(collection)
   i = 0 
+  new_collection = []
   while i < collection.length 
-    new_collection = []
     new_collection << yield(collection[i])
     i += 1 
   end 
